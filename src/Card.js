a8c7ab8828af3554b337c28a13c8d8a39de17b95
ASUS <Yehor Kholodiuk>
2022-04-05 17:37:43 +0300

    initial commit

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {changeStatus} from "./redux/actions";
+import {connect} from "react-redux";
+//import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
+//import mapDispatchToProps from "react-redux/lib/connect/mapDispatchToProps";
 
 const Card = (props) => {
-    const {name,id,description,status,priority} = props.task
+    const {name,_id,description,status,priority} = props.task
     return (
 
             <div className="card">
@@ -9,13 +13,27 @@ const Card = (props) => {
                     <h5 className="card-title">{name}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">{description}</h6>
                     <p>{priority} </p>
-                    <button type="button" className="btn btn-outline-secondary btn-sm"> left</button>
+                    <button type="button" className="btn btn-outline-secondary btn-sm"
+                            disabled = {props.task.status === props.statuses[0].title}
+                    onClick = {()=> props.changeStatus(props.task,props.statuses,-1)}
+                    > left</button>
 
-                    <button type="button" className="btn btn-outline-secondary btn-sm">right </button>
+                    <button type="button" className="btn btn-outline-secondary btn-sm"
+                            disabled = {props.task.status === props.statuses[props.statuses.length - 1].title}
+                            onClick = {()=> props.changeStatus(props.task,props.statuses,+1)}
+                    >right </button>
                 </div>
             </div>
 
     );
 };
 
-export default Card;
+const mapStateToProps = state => ({
+    statuses:state.statuses
+})
+
+const mapDispatchToProps = dispatch => ({
+changeStatus:(card,statuses,direction) => dispatch(changeStatus(card,statuses,direction))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps) (Card);
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -15,4 +15,14 @@ export function getCards(){
     }
 };
 
+export function changeStatus(card, statuses, direction) {
+    const newStatuses = statuses.map(el => el.title)
+    const status = newStatuses[newStatuses.indexOf(card.status)+direction]
+    return (dispatch) => {
+        axios.patch(`http://nazarov-kanban-server.herokuapp.com/card/${card._id}`, {status})
+            .then(res => dispatch(getCards()))
+            .catch(err => console.log('error'))
+    }
+
+}
 
